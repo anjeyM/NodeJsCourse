05afe4a56270d8cf7bda48a5d2a5cb3416abb271
Andrew <Fidel.Marti@yandex.ru>
2022-12-27 17:02:13 +0100

    fix gitignore

--- task-3/src/shared/services/user-service/user.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { BaseUser, User } from '../../types/interfaces';
-export declare const findAll: () => Promise<User[]>;
-export declare const find: (id: string) => Promise<User>;
-export declare const createUser: (user: BaseUser) => Promise<User>;
-export declare const deleteUser: (userId: string) => Promise<User | null>;
-export declare const updateUser: (id: string, user: BaseUser) => Promise<User | null>;
-export declare const getAutoSuggestList: (limit: number) => Promise<User[] | null>;
