79c19236648973925b5875a940e2c04d1599ef30
Andrew <Fidel.Marti@yandex.ru>
2023-01-18 19:52:34 +0100

    fix user service method

--- task-3/src/services/user-service/user.service.ts
+++ task-3/src/services/user-service/user.service.ts
@@ -1,7 +1,7 @@
 import {User} from '../../models/users';
 
 //** Gets limit number of users, sorted by login field. */
-export const getAutoSuggestList = async (limit: number, users: User[]): Promise<User[]|null> => {
+export const getAutoSuggestList = (limit: number, users: User[]): User[]|null => {
   const sortedUsers = Object.values(users).sort(
     (user1: User, user2: User) => user1.login.toLowerCase() > user2.login.toLowerCase() ? 1 : -1);
 
